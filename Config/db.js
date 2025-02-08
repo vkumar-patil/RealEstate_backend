4294c0657a54b97a4ccbce3b884cf2eea030eead
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2025-02-08 19:35:28 +0530

    update with atlas mongo DB

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.mongoDBlocal);
-    console.log(mongoose.connect(process.env.mongoDBlocal));
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(mongoose.connect(process.env.MONGO_URI));
     console.log(`MongoDB Connected`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
