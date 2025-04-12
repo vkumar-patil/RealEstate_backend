64dd1db0bd7464e63392ddd6b9898d55e30bd77c
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2025-04-12 18:29:15 +0530

    update

--- Config/db.js
+++ Config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(mongoose.connect(process.env.MONGO_URI));
-    console.log(`MongoDB Connected`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1); // Exit the process if the connection fails
   }
 };
 
